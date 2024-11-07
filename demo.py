cd277cad2e86e652e388a27708073300ab31fdbb
Ryouko <153080407+Ryouko-Yamanda65777@users.noreply.github.com>
2024-11-07 15:03:09 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -74,6 +74,8 @@ with gr.Blocks(title="🔊 Nex RVC Mobile",theme=gr.themes.Base()) as app:
                                   
               refresh_button = gr.Button("Refresh", variant="primary")
 
+              vc_transform0 = gr.Number(label="Pitch", value=0)
+              
                 
               spk_item = gr.Slider(
 
@@ -93,10 +95,7 @@ with gr.Blocks(title="🔊 Nex RVC Mobile",theme=gr.themes.Base()) as app:
 
                 )
 
-                vc_transform0 = gr.Number(
-                  label="Pitch", 
-                  value=0
-                )
+                
 
                 but0 = gr.Button(value="Convert", variant="primary")
 
